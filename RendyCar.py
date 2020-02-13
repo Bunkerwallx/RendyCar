2c21f0d2b146e8b875d1efe9d9c90d5703220bc6
bunkerwallx <39147256+BUNKERWALLX@users.noreply.github.com>
2020-02-13 05:37:26 -0600

    Add files via upload

--- RendyCar.py
+++ RendyCar.py
@@ -2,8 +2,22 @@
 
 # Bunkerwallx / www.bunkerwallx.com / wwww.lapestenegra.com
 # El contenido de este script es para calculo de rendimiento de combustible  k / l
-print ("Calculo de Rendimiento.")
+
+print ("====================================================================")
+print ("                                                                    ")
+print (" ______              _     _                             _ _        ")
+print ("(____  \            | |   | |                           | | |       ") 
+print (" ____)  )_   _ ____ | |  _| |  _ _____  ____ _ _ _ _____| | | _   _ ") 
+print ("|  __  (| | | |  _ \| |_/ ) |_/ ) ___ |/ ___) | | (____ | | |( \ / )")
+print ("| |__)  ) |_| | | | |  _ (|  _ (| ____| |   | | | / ___ | | | ) X ( ")
+print ("|______/|____/|_| |_|_| \_)_| \_)_____)_|    \___/\_____|\_)_|_/ \_)")
+print ("                                                                    ")
+print ("                                                                    ")
+print ("Que chingue a su madre Trump")
+print ("====================================================================")
+print ("Calculo de Rendimiento Automotriz.")
 print ("www.lapestenegra.com\nwww.bunkerwallx.com\nBy.Bunkerwallx")
+print ("====================================================================")
 kilometros_recorridos = input ("Introduce kilometros recorridos: ")
 kilometros_recorridos = float (kilometros_recorridos)
 litros_consumidos = input ("Introduce los litros consumidos: ")
