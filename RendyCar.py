6a28c313541d3184b1475374141b02da89ae9850
bunkerwallx <39147256+BUNKERWALLX@users.noreply.github.com>
2020-02-13 00:39:02 -0600

    Add files via upload

---
+++ RendyCar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Bunkerwallx / www.bunkerwallx.com / wwww.lapestenegra.com
+# El contenido de este script es para calculo de rendimiento de combustible  k / l
+print ("Calculo de Rendimiento.")
+print ("www.lapestenegra.com\nwww.bunkerwallx.com\nBy.Bunkerwallx")
+kilometros_recorridos = input ("Introduce kilometros recorridos: ")
+kilometros_recorridos = float (kilometros_recorridos)
+litros_consumidos = input ("Introduce los litros consumidos: ")
+litros_consumidos = float (litros_consumidos)
+klc = kilometros_recorridos / litros_consumidos
+print("Kilometros por litro:", klc)
+
